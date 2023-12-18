76f7483e8c66a02990fa00d0a3498388419636ce
J Wokaty <jwokaty@users.noreply.github.com>
2023-12-18 11:06:20 -0500

    Add vignette, list and import

--- R/BEDbaseR.R
+++ R/BEDbaseR.R
@@ -1,3 +1,25 @@
+#' List BED or BEDsets
+#'
+#' @param record_type bed or bedset
+#'
+#' @returns a list of bed or bedsets
+#'
+#' @importFrom glue glue
+#' @importFrom httr2 request req_perform resp_body_json
+#'
+#' @returns a list with metadata
+#'
+#' @examples
+#' recs <- listRecords("bed")
+#'
+#' @export
+listRecords <- function(record_type=c("bed", "bedset"))
+{
+    record_type <- match.arg(record_type)
+    url <- glue("{BEDBaseBaseUrl}/{record_type}/list")
+    req_perform(request(url)) |> resp_body_json()
+}
+
 #' Get metadata
 #'
 #' @param identifier BEDbase record id or object id
@@ -37,6 +59,10 @@ getMetadata <- function(identifier, record_type=c("bed", "bedset", "objects"))
 #'
 #' @examples
 #' filepath <-downloadBedFile("421d2128e183424fcc6a74269bae7934", "/tmp")
+#' filepath <-downloadBedFile("ce9f6e8adf5779edb632245a166b92bd", "/tmp")
+#' zebrafish <-downloadBedFile("eec93c0649bfc2fc90cdc9fca3f52588", "/tmp")
+#' filepath <-downloadBedFile("e6cd0dbb252be66f8c2bdb0d23ed431c", "/tmp")
+#' telomere <-downloadBedFile("b56d842d5727f1cfaaccb0bdc1bba6ad", "/tmp")
 #'
 #' @export
 downloadBedFile <- function(record_id, destdir, access_id="http")
@@ -51,11 +77,37 @@ downloadBedFile <- function(record_id, destdir, access_id="http")
     filepath
 }
 
+#' Create a GRanges object from a BED file
+#'
+#' @param filepath path to a BED file
+#'
+#' @importFrom GenomicRanges GRanges
+#' @importFrom rtracklayer import
+#' @importFrom IRanges IRanges
+#'
+#' @returns a GRanges object
+#'
+#' @examples
+#' grobj <- fileToGRanges("421d2128e183424fcc6a74269bae7934")
+#'
+#' @export
+fileToGRanges <- function(filepath)
+{
+    tryCatch(
+        import(filepath, format = "bed")
+        ,
+        error = function(e) {
+          df <- read.table(filepath)
+          columns <- length(df)
+          stopifnot("File missing required columns" = columns >= 3)
+          GRanges(df[, 1], IRanges(df[, 2], df[, 3]))
+        })
+}
+
+
 #' Download BED file associated with record_id and create a GRanges object
 #'
 #' @param record_id BEDbase record id
-#'
-#' @importFrom rtracklayer import.bed
 #'
 #' @returns a GRanges object
 #'
@@ -65,6 +117,6 @@ downloadBedFile <- function(record_id, destdir, access_id="http")
 #' @export
 importToGRanges <- function(record_id)
 {
-    bedfile <- downloadBedFile(record_id, destdir=tempdir())
-    import.bed(bedfile)
+    filepath <- downloadBedFile(record_id, destdir=tempdir())
+    fileToGRanges(filepath)
 }
