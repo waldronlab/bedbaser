949fc34f39ca48b6b9a8deb61797aefe0041b337
jwokaty <jwokaty@users.noreply.github.com>
2026-02-25 11:24:26 -0500

    Issue/22 (#23)
    * Add test_request
    * Update functions, tests to use test_request
    * Fix .get_extra_cols test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,7 +1,6 @@
 bedbase <- BEDbase(tempdir(), quietly = TRUE)
 ex_bed <- bb_example(bedbase, "bed")
 ex_bedset <- bb_example(bedbase, "bedset")
-ex_bed_md <- bb_metadata(bedbase, ex_bed$id, TRUE)
 
 test_that(".get_file_name returns file name", {
     expect_equal(
@@ -10,24 +9,20 @@ test_that(".get_file_name returns file name", {
     )
 })
 
-test_that(".get_url returns a url", {
-    file_url <- .get_url(ex_bed_md, "http")
-    expect_true(stringr::str_detect(file_url, "(https?|ftp|s3)://"))
-})
-
 test_that(".get_file returns a valid file path", {
-    file_path <- .get_file(ex_bed_md, tempdir(), "http")
+    file_path <- .get_file(bedbase, ex_bed$id, tempdir())
     expect_true(file.exists(file_path))
-    file_path <- .get_file(ex_bed_md, getCache(bedbase, "bedfiles"), "http")
+    file_path <- .get_file(bedbase, ex_bed$id, getCache(bedbase, "bedfiles"))
     expect_true(file.exists(file_path))
 })
 
 test_that(".get_extra_cols returns a named vector", {
-    ex_bed_md2 <- bb_metadata(bedbase, ex_bedset$bed_ids[[1]], TRUE)
-    file_path <- .get_file(ex_bed_md2, getCache(bedbase, "bedfiles"), "http")
-    x_y <- strsplit(gsub("bed", "", ex_bed_md2$bed_compliance),
+    id <- ex_bedset$bed_ids[[1]]
+    file_path <- .get_file(bedbase, id, getCache(bedbase, "bedfiles"))
+    ex_bed_md <- bb_metadata(bedbase, id, TRUE)
+    x_y <- strsplit(gsub("bed", "", ex_bed_md$bed_compliance),
                     "+",
-                    fixed= TRUE)[[1]]
+                    fixed = TRUE)[[1]]
     extra_cols <- .get_extra_cols(file_path,
                                   as.numeric(x_y[1]),
                                   as.numeric(x_y[2]))
