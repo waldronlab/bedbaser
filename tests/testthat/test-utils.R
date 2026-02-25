9a1c910db4eb5d98a40ccabeb69d048edf5a8d07
A Wokaty <awokaty@users.noreply.github.com>
2026-02-25 11:08:27 -0500

    Fix .get_extra_cols test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,7 +1,6 @@
 bedbase <- BEDbase(tempdir(), quietly = TRUE)
 ex_bed <- bb_example(bedbase, "bed")
 ex_bedset <- bb_example(bedbase, "bedset")
-ex_bed_md <- bb_metadata(bedbase, ex_bed$id, TRUE)
 
 test_that(".get_file_name returns file name", {
     expect_equal(
@@ -18,12 +17,12 @@ test_that(".get_file returns a valid file path", {
 })
 
 test_that(".get_extra_cols returns a named vector", {
-    ids <- ex_bedset$bed_ids[[1]]
-    file_path <- .get_file(bedbase, ids, getCache(bedbase, "bedfiles"))
-    ex_bed_md2 <- bb_metadata(bedbase, ids, TRUE)
-    x_y <- strsplit(gsub("bed", "", ex_bed_md2$bed_compliance),
+    id <- ex_bedset$bed_ids[[1]]
+    file_path <- .get_file(bedbase, id, getCache(bedbase, "bedfiles"))
+    ex_bed_md <- bb_metadata(bedbase, id, TRUE)
+    x_y <- strsplit(gsub("bed", "", ex_bed_md$bed_compliance),
                     "+",
-                    fixed= TRUE)[[1]]
+                    fixed = TRUE)[[1]]
     extra_cols <- .get_extra_cols(file_path,
                                   as.numeric(x_y[1]),
                                   as.numeric(x_y[2]))
